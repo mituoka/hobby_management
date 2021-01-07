d80a476c54ff7bd11e69bbddd5606d7b6656b9b1
光岡知洋 <mitsuokatomohiro@MacPro.local>
2021-01-07 09:09:36 +0900

    メニュー画面繋ぎ込み対応

--- hobby_management/login_app/urls.py
+++ hobby_management/login_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import login
 
 urlpatterns = [
-    path('', login.main_login, name='test'),
-    path('login/', login.main_login, name="test"),
+    # path('', login.main_login, name='test'),
+    path('login/', login.main_login, name="login"),
+    path('menu/', login.login_bottom_push, name="menu"),
 ]
--- hobby_management/login_app/views/login.py
+++ hobby_management/login_app/views/login.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.urls import reverse
 
 def main_login(request):
 
@@ -11,5 +11,24 @@ def main_login(request):
         print(10)
     elif (request.method == 'POST'):
         print(20)
+        redirect_url = reverse('menu')
+        
+        url = f'{redirect_url}'
+        print(redirect_url)
+        print(url)
+        return redirect('menu')
 
     return render(request, 'login.html',param)
+
+def login_bottom_push(request):
+    
+    param = {
+        'a':'1',
+    }
+
+    if (request.method == 'GET'):
+        print(30)
+    elif (request.method == 'POST'):
+        print(40)
+
+    return render(request, 'menu.html',param)
