fc000b6043e4404ec3a003c51c654e45b62a1fd1
光岡知洋 <mitsuokatomohiro@MacPro.local>
2021-01-10 00:31:53 +0900

    配置調整

--- hobby_management/hobby_management/settings.py
+++ hobby_management/hobby_management/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'hobby_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'user_info', #　作成したデータベース名
+        'USER': 'root', # ログインユーザー名
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
